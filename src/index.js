6a1be112d9854a6e29a4e937eb1a55a4b2c48fdb
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-11-17 09:55:28 +0100

    feat: assign variables to form elements

--- src/index.js
+++ src/index.js
@@ -3,3 +3,15 @@ console.log("hello");
 if (process.env.NODE_ENV !== "production") {
 	console.log("Looks like we are in development mode!");
 }
+const jsForm = document.querySelectorAll(".js-form");
+function assignVariables(arr) {
+	const elements = {};
+	for (const el of arr) {
+		if (!(el.id in elements)) {
+			elements[el.id] = el;
+		}
+	}
+	return elements;
+}
+
+assignVariables(jsForm);
