d2380d42fe6ac53f1dc0ee1f2a662e5e142e39e5
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-11-17 14:08:16 +0100

    feat: add custom validation messages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import "./style.css";
-console.log("hello");
+import { postalCodes } from "./postcodes";
 if (process.env.NODE_ENV !== "production") {
 	console.log("Looks like we are in development mode!");
 }
 const jsForm = document.querySelectorAll(".js-form");
+const errorMsgs = {
+	badInput: "Bad input",
+	patternMismatch: "Pattern mismatch.",
+	rangeOverflow: "Number too large.",
+	rangeUnderflow: "Number too small.",
+	stepMismatch: "Step mismatch.",
+	tooLong: "Text too long.",
+	tooShort: "Text too short.",
+	typeMismatch: "Wrong type.",
+	valueMissing: "This field is required. Please fill it in.",
+};
 function assignVariables(arr) {
 	const elements = {};
 	for (const el of arr) {
@@ -14,49 +25,66 @@ function assignVariables(arr) {
 	return elements;
 }
 
-const errorMsgs = {
-	badInput: "Bad input",
-	customError: "Custom error",
-	patternMismatch: "Pattern mismatch",
-	rangeOverflow: "Number too large",
-	rangeUnderflow: "Number too small",
-	stepMismatch: "Step mismatch",
-	tooLong: "Text too long",
-	tooShort: "Text too short",
-	typeMismatch: "Wrong type",
-	valueMissing: "Input missing",
-};
+function setPostcodePattern() {
+	const postCountry = postalCodes
+		.find((postal) => postal.abbrev === country.value)
+		.regex.replaceAll("/", "");
+	console.log(postCountry);
+	postcode.setAttribute("pattern", postCountry);
+}
+
+function setPasswordPattern() {
+	confirmPassword.setAttribute("pattern", password.value);
+}
+
+window.onload(assignVariables(jsForm));
 
 form.addEventListener("focusout", (e) => {
 	const target = e.target;
-	let errorMsg = "";
-	const validityObject = target.validity;
-	const keys = Object.keys(errorMsgs);
-	const filtered = keys.filter((key) => validityObject[key]);
-	for (const element of filtered) {
-		errorMsg += `${errorMsgs[`${element}`]}. `;
-	}
-	switch (target.id) {
-		case "email":
-			break;
-		case "country":
-			break;
-		case "postcode":
-			break;
-		case "password":
-			break;
-		case "confirmPassword":
-			break;
+	if (target.id === "resetBtn" || target === "submitBtn") {
+		void 0;
+	} else {
+		if (target.checkValidity() || target.validity.valid) {
+			target.setCustomValidity("");
+			const targetTick = `${target.id}Tick`;
+			const targetTickObject = document.getElementById(targetTick);
+			targetTickObject.classList.remove("hidden");
+			if (target === country) {
+				setPostcodePattern();
+			}
+			if (target === password) {
+				setPasswordPattern();
+			}
+		} else {
+			let errorMsg = "";
+			const keys = Object.keys(errorMsgs);
+			const filtered = keys.filter((key) => target.validity[key]);
+			for (const element of filtered) {
+				errorMsg += `${errorMsgs[`${element}`]} `;
+			}
+
+			switch (target.id) {
+				case "email":
+					errorMsg +=
+						"Email address must have the format email@provider.domain.";
+					break;
+				case "country":
+					setPostcodePattern();
+					errorMsg += "Please select a country from the dropdown list.";
+					break;
+				case "postcode":
+					errorMsg = "Postcode format is invalid for your country.";
+					break;
+				case "password":
+					errorMsg +=
+						"Password must be between 8 and 64 characters and contain at least one uppercase letter, one lowercase letter, and one special character.";
+					break;
+				case "confirmPassword":
+					errorMsg += "Passwords don't match.";
+					break;
+			}
+			target.setCustomValidity(errorMsg);
+			target.reportValidity();
+		}
 	}
 });
-
-//const validityObject = email.validity;
-//console.log(validityObject);
-//
-//console.log(keys);
-//
-//console.log(filtered);
-//
-//console.log(errorMsg);
-
-assignVariables(jsForm);
