a1e03d09b684f998eab19e09f3dc39e90d51df5e
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-11-17 11:15:56 +0100

    feat: add generic error message

--- src/index.js
+++ src/index.js
@@ -14,4 +14,49 @@ function assignVariables(arr) {
 	return elements;
 }
 
+const errorMsgs = {
+	badInput: "Bad input",
+	customError: "Custom error",
+	patternMismatch: "Pattern mismatch",
+	rangeOverflow: "Number too large",
+	rangeUnderflow: "Number too small",
+	stepMismatch: "Step mismatch",
+	tooLong: "Text too long",
+	tooShort: "Text too short",
+	typeMismatch: "Wrong type",
+	valueMissing: "Input missing",
+};
+
+form.addEventListener("focusout", (e) => {
+	const target = e.target;
+	let errorMsg = "";
+	const validityObject = target.validity;
+	const keys = Object.keys(errorMsgs);
+	const filtered = keys.filter((key) => validityObject[key]);
+	for (const element of filtered) {
+		errorMsg += `${errorMsgs[`${element}`]}. `;
+	}
+	switch (target.id) {
+		case "email":
+			break;
+		case "country":
+			break;
+		case "postcode":
+			break;
+		case "password":
+			break;
+		case "confirmPassword":
+			break;
+	}
+});
+
+//const validityObject = email.validity;
+//console.log(validityObject);
+//
+//console.log(keys);
+//
+//console.log(filtered);
+//
+//console.log(errorMsg);
+
 assignVariables(jsForm);
